8c87e8effc52b5b87386e537a345f77bdfb1d75c
pangphaichit <pg_grj@hotmail.com>
2024-12-24 13:46:36 +0700

    submit answers

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+O(n) because the loop iterates through every element in the array.
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+ O(log n) because the loop runs a maximum of log₂n times.
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+findProductPrice because this function has lower time complexity.
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
-
+O(n²) because of nested loop, the loop iterates through both outer and inner loops, resulting which in n squared.
 
 */
